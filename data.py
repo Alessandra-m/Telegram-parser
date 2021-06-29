ec9543bfb16f53a699545e7cb659798f72c15968
Alessandra-m <alessandra.myzina.s@gmail.com>
2021-06-29 21:06:18 +0300

    Парсер из канала на канал

---
+++ data.py
@@ -0,0 +1,4 @@
+api_id = 6262981
+api_hash = 'a354281220e0be50000c7c90b3434f9e'
+api_client = 'me_parser'
+channels = ['EUC_market']
---
+++ main.py
@@ -0,0 +1,14 @@
+from telethon import TelegramClient, events
+from data import api_id, api_hash, api_client, channels
+
+client = TelegramClient('Me', api_id, api_hash)
+client.start()
+
+@client.on(events.NewMessage)
+async def event_handler(event):
+    if event.chat.username in channels:
+       chat = await event.get_input_chat()
+       msg = await client.get_messages(chat.channel_id, limit=1)
+       await client.forward_messages(api_client, msg)
+
+client.run_until_disconnected()
