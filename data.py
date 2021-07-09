1a086a12647e624c0549284cbe78660a60036499
Alessandra-m <alessandra.myzina.s@gmail.com>
2021-07-09 17:48:04 +0300

    full price

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 api_id = 6262981
 api_hash = 'a354281220e0be50000c7c90b3434f9e'
 api_client = 'me_parser'
-channels = ['parser_test1']
+channels = ['EUC_market', 'parser_test1']
 
 #'EUC_market',
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sqlite3
 import re
+import datetime
 from telethon import TelegramClient, events
 from telethon.sync import TelegramClient
 from telethon.tl.types import MessageFwdHeader
@@ -21,6 +22,9 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS database(
     """)
 connection.commit()
 
+date = datetime.datetime.today()
+last_date = date - datetime.timedelta(days = 180)
+    
 
 #Создание клиента
 client = TelegramClient('Me', api_id, api_hash)
@@ -31,6 +35,7 @@ client.start()
 
 #Обработка сообщений
 async def event_handler(event):
+
     if event.chat.username in channels:
        chat = await event.get_input_chat() #Данные канала из которого пришло сообщение
        msg = await client.get_messages(chat.channel_id, limit = 1) #Последние сообщение
@@ -40,10 +45,9 @@ async def event_handler(event):
     cursor.execute('DELETE FROM database')
 
     async for message in client.iter_messages(chat, reverse = True):
-
         text = message.text
         user_id = message.id
-        date = message.date
+        date_1 = message.date
 
         hashtag = str()
         text_str = str(text)
@@ -51,29 +55,22 @@ async def event_handler(event):
         for i in range(0,len(tags)):
             hashtag = hashtag + tags[i] + ' '
 
-        
         price = str()
         text_str = text_str.lower()
 
         if re.findall(r'(цена+[ :-]+\d+)', text_str) != []:
             price = re.findall(r'(цена+[ :-]+\d+)', text_str)
 
-        elif re.findall(r'(\d+[ ]+[₽])', text_str) != []:
-            price = re.findall(r'(\d+[ ]+[₽])', text_str)
-        
-        elif re.findall(r'(\d+[₽])', text_str) != []:
-            price = re.findall(r'(\d+[₽])', text_str)
+        elif  re.findall(r'(\d+[ ]+[₽рт])', text_str) != []:
+            price = re.findall(r'(\d+[ ]+[₽рт])', text_str)
 
-        elif  re.findall(r'(\d+[ ]+[РртТ])', text_str) != []:
-            price = re.findall(r'(\d+[ ]+[РртТ])', text_str)
+        elif re.findall(r'(\d+[₽рт])', text_str) != []:
+            price = re.findall(r'(\d+[₽рт])', text_str)
 
-        elif re.findall(r'(\d+[РртТ])', text_str) != []:
-            price = re.findall(r'(\d+Р[ртТ])', text_str)
-
-        elif  re.findall(r'(\d+[ ]+[т]+[ы])', text_str) != []:
+        elif  re.findall(r'(\d+[ ]+[т]+[ы]+[c])', text_str) != []:
             price = re.findall(r'(\d+[ ]+[т]+[ы]+[c])', text_str)
 
-        elif re.findall(r'(\d+[т]+[ы])', text_str) != []:
+        elif re.findall(r'(\d+[т]+[ы]+[c])', text_str) != []:
             price = re.findall(r'(\d+[т]+[ы]+[c])', text_str)
         
         elif  re.findall(r'(\d+[ ]+[кk]+[ .])', text_str) != []:
@@ -81,6 +78,12 @@ async def event_handler(event):
 
         elif re.findall(r'(\d+[кk]+[ .])', text_str) != []:
             price = re.findall(r'(\d+[кk]+[ .])', text_str)
+
+        elif re.findall(r'(\d+[кk]+\Z)', text_str) != []:
+            price = re.findall(r'(\d+[кk]+\Z)', text_str)
+
+        elif re.findall(r'(\d+[ ]+[кk]+\Z)', text_str) != []:
+            price = re.findall(r'(\d+[ ]+[кk]+\Z)', text_str)
         
         elif  re.findall(r'(\d+[ ]+[т]+[р])', text_str) != []:
             price = re.findall(r'(\d+[ ]+[т]+[р])', text_str)
@@ -91,18 +94,19 @@ async def event_handler(event):
         res_price = str()
         res_p = str()
         for i in range(0,len(price)):
-            res_price = str(res_price + price[i] + ' ')
-        res_price = re.findall(r'(\d+)',res_price)
+            res_price = res_price + price[i] + ' '
+        res_price = re.findall(r'(\d+)', res_price)
         for i in range(0,len(res_price)):
-            res_p = str(res_p + res_price[i] + ' ')
-
-
-        cursor.execute('INSERT INTO database (user_id, text, date, sharp, price) VALUES (?, ?, ?, ?, ?)', (user_id, text, date, hashtag, res_p))
-        connection.commit()
+            res_p = res_p + res_price[i] + ' '
         
-    
+        if len(res_p) <= 3 and len(res_p) > 0:
+            res_p = res_p + '000'
+        if len(res_p) > 0:
+            res_p = res_p + '₽'
+        res_p = re.sub(r'\s+', '', res_p)
 
-
+        cursor.execute('INSERT INTO database (user_id, text, date, sharp, price) VALUES (?, ?, ?, ?, ?)', (user_id, text, date_1, hashtag, res_p))
+        connection.commit()
 
 client.run_until_disconnected()
 
