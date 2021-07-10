a8cc1f173ffa60e40516b297b8197b645642660b
Alessandra-m <alessandra.myzina.s@gmail.com>
2021-07-11 00:10:04 +0300

    class db

--- 1.py
+++ 1.py
@@ -1,7 +1,9 @@
+import tkinter as tk
 import sqlite3
 import re
 import datetime
 
+from tkinter import ttk
 from telethon import TelegramClient, events
 from telethon.sync import TelegramClient
 from telethon.tl.types import MessageFwdHeader
@@ -9,19 +11,29 @@ from telethon.tl.types.messages import ChatsSlice
 
 from  data import api_id, api_hash, api_client, channels
 
-connection = sqlite3.connect('database.db')
+class DB:
+    def __init__(self):
+        self.conn = sqlite3.connect('database.db')
+        self.c = self.conn.cursor()
+        self.c.execute("""CREATE TABLE IF NOT EXISTS database(
+            user_id INTEGER,
+            text TEXT,
+            date DATE,
+            price TEXT,
+            URL TEXT,
+            sharp TEXT);
+            """)
+        self.conn.commit()
 
-cursor = connection.cursor()  
+    def insert_data(self, user_id, text, date_1, hashtag, res_p):
+        self.c.execute('INSERT INTO database (user_id, text, date, sharp, price) VALUES (?, ?, ?, ?, ?)', (user_id, text, date_1, hashtag, res_p))
+        self.conn.commit()
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS database(
-    user_id INTEGER,
-    text TEXT,
-    date DATE,
-    price TEXT,
-    URL TEXT,
-    sharp TEXT);
-    """)
-connection.commit()
+    def delete_data(self):
+        self.c.execute('DELETE FROM database')
+        self.conn.commit()
+
+db = DB()
 
 date = datetime.datetime.today()
 last_date = date - datetime.timedelta(days = 180)
@@ -36,8 +48,8 @@ async def event_handler(event):
        chat = await event.get_input_chat()
        msg = await client.get_messages(chat.channel_id, limit = 1) 
        await client.forward_messages(api_client, msg) 
-
-    cursor.execute('DELETE FROM database')
+    
+    db.delete_data()
 
     async for message in client.iter_messages(chat, reverse = True, offset_date = last_date):
         text = message.text
@@ -94,9 +106,7 @@ async def event_handler(event):
             res_p = res_p + '₽'
         res_p = re.sub(r'\s+', '', res_p)
 
-        cursor.execute('INSERT INTO database (user_id, text, date, sharp, price) VALUES (?, ?, ?, ?, ?)', (user_id, text, date_1, hashtag, res_p))
-        connection.commit()
+        db.insert_data(user_id, text, date_1, hashtag, res_p)
 
 client.run_until_disconnected()
 
-connection.close()
--- data.py
+++ data.py
@@ -1,6 +1,12 @@
+#Данные для создания клиента
 api_id = 6262981
 api_hash = 'a354281220e0be50000c7c90b3434f9e'
-api_client = 'me_parser'
-channels = ['EUC_market', 'parser_test1']
 
-#'EUC_market',
+api_client = 'me_parser'
+channels = ['parser_test1']
+
+#Данные для создания бота
+TOKEN = "1703340370:AAHO-kzP-4v5gTTc4CTzPjT89jw_vtcCglA"
+
+
+#'EUC_market', 
